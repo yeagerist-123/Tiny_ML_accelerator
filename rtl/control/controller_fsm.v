50eb2537fd3c7b45e73683660ffd1a08747946cb
yeagerist-123 <koushik.pedaprolu@gmail.com>
2026-04-02 01:49:12 +0530

    Create controller_fsm.v

---
+++ rtl/control/controller_fsm.v
@@ -0,0 +1,28 @@
+module controller_fsm (
+    input clk, reset, start,
+    output reg load_weights,
+    output reg done
+);
+
+    reg [4:0] count;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            count <= 0;
+            load_weights <= 0;
+            done <= 0;
+        end else begin
+            load_weights <= 0;
+            done <= 0;
+
+            if (start) begin
+                load_weights <= 1;
+                count <= 0;
+            end else if (count < 10) begin
+                count <= count + 1;
+            end else begin
+                done <= 1;
+            end
+        end
+    end
+endmodule
