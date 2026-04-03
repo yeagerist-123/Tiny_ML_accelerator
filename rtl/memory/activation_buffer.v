09598bcc85e9f0ee37bd0aee520186707ea496b8
yeagerist-123 <koushik.pedaprolu@gmail.com>
2026-04-03 16:04:48 +0530

    Update activation_buffer.v

--- rtl/memory/activation_buffer.v
+++ rtl/memory/activation_buffer.v
@@ -1,5 +1,6 @@
 module activation_buffer (
     input clk,
+    input reset,
     input write_en,
     input [1:0] addr,
     input [7:0] data_in,
@@ -9,9 +10,18 @@ module activation_buffer (
     reg [7:0] mem [0:3];
 
     always @(posedge clk) begin
-        if (write_en)
+        if (reset) begin
+            mem[0] <= 0;
+            mem[1] <= 0;
+            mem[2] <= 0;
+            mem[3] <= 0;
+        end else if (write_en) begin
             mem[addr] <= data_in;
-
-        act_bus <= {mem[3], mem[2], mem[1], mem[0]};
+        end
     end
+
+    always @(*) begin
+        act_bus = {mem[3], mem[2], mem[1], mem[0]};
+    end
+
 endmodule
