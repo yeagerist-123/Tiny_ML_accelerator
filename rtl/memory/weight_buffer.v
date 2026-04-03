8aebc2f79551f11e0efc05d73b59a893297d4455
yeagerist-123 <koushik.pedaprolu@gmail.com>
2026-04-03 16:05:07 +0530

    Update weight_buffer.v

--- rtl/memory/weight_buffer.v
+++ rtl/memory/weight_buffer.v
@@ -1,20 +1,23 @@
 module weight_buffer (
-    input clk,
-    input write_en,
+    input clk, reset, write_en,
     input [3:0] addr,
     input [7:0] data_in,
     output reg [127:0] weight_bus
 );
-
     reg [7:0] mem [0:15];
-
     integer i;
 
     always @(posedge clk) begin
-        if (write_en)
+        if (reset) begin
+            for (i = 0; i < 16; i = i + 1) mem[i] <= 0;
+        end else if (write_en) begin
             mem[addr] <= data_in;
+        end
+    end
 
-        for (i=0;i<16;i=i+1)
-            weight_bus[i*8 +: 8] <= mem[i];
+    // Continuous update for the compute engine
+    always @(*) begin
+        for (integer k = 0; k < 16; k = k + 1)
+            weight_bus[k*8 +: 8] = mem[k];
     end
 endmodule
