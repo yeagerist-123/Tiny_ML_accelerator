45ca638b16ad892a7c2adbe49541af7c50fa9a9c
yeagerist-123 <koushik.pedaprolu@gmail.com>
2026-04-03 16:03:47 +0530

    Update relu.v

--- rtl/compute/relu.v
+++ rtl/compute/relu.v
@@ -2,7 +2,9 @@ module relu (
     input signed [23:0] data_in,
     output [7:0] data_out
 );
-    assign data_out = (data_in < 0) ? 0 :
+
+    assign data_out = (data_in < 0) ? 8'd0 :
                       (data_in > 255) ? 8'd255 :
                       data_in[7:0];
+
 endmodule
